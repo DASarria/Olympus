c6e6d291323b2586cc2c94c0617410acccfa8557
hakki17 <mariapaulasm2018@gmail.com>
2025-05-21 18:29:54 -0500

    se hicieron cambios de solo una interfaz que cambia dependiendo el rol y no varias interfaces, se quito el buscador de las clases porque estaba fallando

--- pages/clasesExtracurriculares/CEAdminPrincipal.tsx
+++ pages/clasesExtracurriculares/CEAdminPrincipal.tsx
@@ -6,54 +6,75 @@ import estadisticasCE from "../../assets/images/clasesExtracurriculares/estadist
 import horariosCE from "../../assets/images/clasesExtracurriculares/horariosCE.png";
 
 const ClasesExtracurriculares = () => {
+  const userRole = "administrador";
+  
   const router = useRouter();
-
   const navegarA = (ruta) => {
     router.push(ruta);
   };
 
+  const isAdmin = userRole === "administrador" || userRole === "profesor";
+  
   return (
     <div className="flex flex-col h-full bg-white-50">
-      {/* Banner principal */}
       <div className="bg-red-800 text-white p-4 m-4 rounded-lg shadow-md">
         <h2 className="font-montserrat font-bold text-2xl mb-1">Clases Extracurriculares</h2>
         <p className="font-open-sans text-1xl font-normal">
           Consulta tu asistencia a actividades deportivas, culturales, de relajación o desarrollo personal, que aporten con tu desarrollo personal y físico.
         </p>
       </div>
-
-      {/* Contenedor de tarjetas */}
+      
       <div className="grid grid-cols-1 md:grid-cols-6 gap-5 p-1">
-        {/* Tarjeta de Historial de Clases */}
         <div
           className="bg-white rounded-lg shadow-md p-5 h-50 flex flex-col items-center justify-center hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 cursor-pointer border border-gray-100"
-          onClick={() => navegarA('/clasesExtracurriculares/CEAdminClases')}
+          onClick={() => navegarA('/clasesExtracurriculares/CEProfeEstudianteClases')}
         >
           <div className="bg-white-50 p-2 rounded-full mb-6 shadow-inner">
             <Image
               src={clasesCE}
               width={64}
               height={64}
+              alt="Clases"
             />
           </div>
-          <h3 className="font-montserrat font-bold text-1xl text-center">Historial Clases</h3>
+          <h3 className="font-montserrat font-bold text-1xl text-center">
+            {isAdmin ? "Clases y asistencias" : "Mis Clases"}
+          </h3>
         </div>
-
-        {/* Tarjeta de Estadísticas de Clases */}
-        <div
-          className="bg-white rounded-lg shadow-md p-8 h-50 flex flex-col items-center justify-center hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 cursor-pointer border border-gray-100"
-          onClick={() => navegarA('/clasesExtracurriculares/estadisticas')}
-        >
-          <div className="bg-white-50 p-2 rounded-full mb-6 shadow-inner">
-            <Image
-              src={estadisticasCE}
-              alt="Estadísticas de Clases"
-              width={64}
-              height={64}
-            />
+        
+        {isAdmin && (
+          <div
+            className="bg-white rounded-lg shadow-md p-8 h-50 flex flex-col items-center justify-center hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 cursor-pointer border border-gray-100"
+            onClick={() => navegarA('/clasesExtracurriculares/CEProfeEstudianteClases')}
+          >
+            <div className="bg-white-50 p-2 rounded-full mb-6 shadow-inner">
+              <Image
+                src={estadisticasCE}
+                alt="Estadísticas de Clases"
+                width={64}
+                height={64}
+              />
+            </div>
+            <h3 className="font-montserrat font-bold text-1xl text-center">Estadísticas Clases</h3>
           </div>
-          <h3 className="font-montserrat font-bold text-1xl text-center">Estadísticas Clases</h3>
-        </div>
+        )}
+        
+        {isAdmin && (
+          <div
+            className="bg-white rounded-lg shadow-md p-8 h-50 flex flex-col items-center justify-center hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 cursor-pointer border border-gray-100"
+            onClick={() => navegarA('/clasesExtracurriculares/CEProfeEstudianteClases')}
+          >
+            <div className="bg-white-50 p-2 rounded-full mb-6 shadow-inner">
+              <Image
+                src={horariosCE}
+                alt="Gestión de Clases"
+                width={64}
+                height={64}
+              />
+            </div>
+            <h3 className="font-montserrat font-bold text-1xl text-center">Gestión Clases</h3>
+          </div>
+        )}
       </div>
     </div>
   );
