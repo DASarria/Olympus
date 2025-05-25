6c54813a9fed6713678587dfff36ce68e5aa81cf
David <davidsarria911@hotmail.com>
2025-05-25 16:21:46 -0500

    Editando proteccion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
-import ProtectedRoute from "@/components/ProtectedRoute";
 
 
 
@@ -16,27 +15,12 @@ export default function App({ Component, pageProps }: AppProps) {
 
   // Muestro el layout solo si no estamos en la página de login, se pueden poner mas rutas pero por el momento dejé esas
   const isAuthPage = ['/', '/login'].includes(router.pathname);
-
-  const rolesPermitidosPorRuta:Record<string,string[]> = {
-    '/salasCrea/InicioSalasCreaADMIN':['SALA_ADMIN'],
-    '/salasCrea/SCERAdmin':['SALA_ADMIN'],
-    '/salasCrea/SCPAdmin':['SALA_ADMIN'],
-    '/salasCrea/SCRAdmin':['SALA_ADMIN'],
-    '/Dashboard':['SALA_ADMIN','STUDENT'],
-    '/salasCrea/InicioSalasCreaUsuario':['STUDENT'],
-    '/salasCrea/ReservUser':['STUDENT'],
-    '/salasCrea/ReservOwnUser':['STUDENT'],
-  };
-
-  const allowedRoles = rolesPermitidosPorRuta[router.pathname];
   
   return isAuthPage ? (
       <Component {...pageProps} />
   ) : (
-        <ProtectedRoute allowedRoles={allowedRoles}>
-          <Layout notificationsCount={0}>
-            <Component {...pageProps} />
-          </Layout>
-        </ProtectedRoute>
+        <Layout notificationsCount={0}>
+          <Component {...pageProps} />
+        </Layout>
   );
 }
