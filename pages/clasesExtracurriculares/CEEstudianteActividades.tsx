a6194dcc7990d4347401af63194eb3a0fcddc38a
Alejandro PR <samuel.prieto-r@mail.escuelaing.edu.co>
2025-05-25 20:00:40 -0500

    Agregando todo

--- pages/clasesExtracurriculares/CEEstudianteActividades.tsx
+++ pages/clasesExtracurriculares/CEEstudianteActividades.tsx
@@ -7,7 +7,32 @@ const borderRadius = "rounded-2xl";
 const divGeneral = " w-[75vw] h-[75vh] flex flex-col  mt-[4vh] items-center justify-center";
 const titulo = "w-full h-[10%] flex items-center justify-start p-4 box-border";
 const h1 = "text-[#990000] text-4xl font-semibold text-left";
-const divComponentes = "w-full h-full overflow-x-auto overflow-y-auto px-4 py-2 flex flex-col sm:flex-row gap-4 items-center scrollbar-custom";
+
+
+
+const divComponentes = `
+  w-full 
+  flex-1 
+  overflow-y-auto 
+  px-6 
+  py-4 
+  grid 
+  grid-cols-1 
+  sm:grid-cols-2 
+  lg:grid-cols-3 
+  xl:grid-cols-4 
+  gap-6 
+  auto-rows-max
+  scrollbar-custom
+  content-start
+`;
+
+
+
+
+
+
+
 
 
 interface Actividad {
@@ -60,7 +85,7 @@ const CEEstudianteActividades = () => {
 
 
     const tokenJWT =
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEyMTMyMTQxIiwidXNlck5hbWUiOiJhZG1pbiIsImVtYWlsIjoiYWRtaW5AZXNjdWVsYWluZy5lZHUuY28iLCJuYW1lIjoiZWwgYWRtaW4iLCJyb2xlIjoiQURNSU4iLCJzcGVjaWFsdHkiOiJudWxsIiwiZXhwIjoxNzQ4MDQzMzcwfQ.qRQ638qL8IGyG2OxPO3I3ygPrX0c1u2QZhUwdwyq7_o";
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEyMyIsInVzZXJOYW1lIjoiYWRtaW4iLCJlbWFpbCI6ImFkbWluQGVzY3VlbGFpbmcuZWR1LmNvIiwibmFtZSI6ImVsIGFkbWluIiwicm9sZSI6IkFETUlOIiwic3BlY2lhbHR5IjoibnVsbCIsImV4cCI6MTc0ODIyNDcwM30.7H4Kq9rbzAAbuqkXQtvVJBHZvygXW-JXjWUbVhsN1Ys";
 
     const linkAPI =
         "https://hadesback-app-c5fwbybjd0gnf0fx.canadacentral-01.azurewebsites.net";
@@ -117,6 +142,36 @@ const CEEstudianteActividades = () => {
             </div>
             <div className={divComponentes}>
                 {actividades.map((actividad:Tarjeta) => (
+                    <ActividadTarjeta
+                    key={actividad.id}
+                    activityType={actividad.activityType}
+                    fecha={actividad.date}
+                    startHour={actividad.startHour}
+                    endHour={actividad.endHour}
+                    asiste={actividad.asiste}
+                    />
+                    ))}
+                    {actividades.map((actividad:Tarjeta) => (
+                    <ActividadTarjeta
+                    key={actividad.id}
+                    activityType={actividad.activityType}
+                    fecha={actividad.date}
+                    startHour={actividad.startHour}
+                    endHour={actividad.endHour}
+                    asiste={actividad.asiste}
+                    />
+                    ))}
+                    {actividades.map((actividad:Tarjeta) => (
+                    <ActividadTarjeta
+                    key={actividad.id}
+                    activityType={actividad.activityType}
+                    fecha={actividad.date}
+                    startHour={actividad.startHour}
+                    endHour={actividad.endHour}
+                    asiste={actividad.asiste}
+                    />
+                    ))}
+                    {actividades.map((actividad:Tarjeta) => (
                     <ActividadTarjeta
                     key={actividad.id}
                     activityType={actividad.activityType}
